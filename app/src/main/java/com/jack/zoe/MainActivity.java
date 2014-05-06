4fee66ea140a3e290477f66365afc5676aaa5bdf
Jack Tsai <wa0327@gmail.com>
2014-05-06 14:30:32 +0800

    first commit

---
+++ app/src/main/java/com/jack/zoe/MainActivity.java
@@ -0,0 +1,79 @@
+package com.jack.zoe;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    private TimerTask pictureRollingTask;
+    private MessageAnimation messageAnimator;
+    private MediaPlayer mp3Player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_main);
+
+        this.startRollingPicture();
+
+        this.messageAnimator = new MessageAnimation(this);
+        this.messageAnimator.start();
+
+        this.mp3Player = MediaPlayer.create(this, R.raw.dance_of_the_dragonfly);
+        this.mp3Player.setLooping(true);
+        this.mp3Player.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.mp3Player.stop();
+        this.mp3Player.release();
+        this.messageAnimator.cancel();
+        this.endRollingPicture();
+        super.onDestroy();
+    }
+
+    private void startRollingPicture() {
+        this.pictureRollingTask = new TimerTask() {
+            private int[] picArray = new int[] { R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j };
+            private int currentIndex = 0;
+            private ImageView zoePicture = (ImageView)MainActivity.this.findViewById(R.id.zoePicture);
+
+            private Runnable runnable = new Runnable() {
+                public void run() {
+                    displayPicture();
+                    prepareNextPicture();
+                }
+
+                private void displayPicture() {
+                    zoePicture.setImageResource(picArray[currentIndex]);
+                }
+
+                private void prepareNextPicture() {
+                    currentIndex = currentIndex + 1;
+                    if ( currentIndex >= picArray.length) {
+                        currentIndex = 0;
+                    }
+                }
+            };
+
+            @Override
+            public void run() {
+                MainActivity.this.runOnUiThread(runnable);
+            }
+        };
+
+        new Timer().schedule(pictureRollingTask, 100, 3000);
+    }
+
+    private void endRollingPicture() {
+        this.pictureRollingTask.cancel();
+    }
+}
---
+++ app/src/main/java/com/jack/zoe/MessageAnimation.java
@@ -0,0 +1,129 @@
+package com.jack.zoe;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.widget.TextView;
+import android.widget.Toast;
+
+class MessageAnimation {
+	
+	class MessagePair {
+		public String Message;
+		public TextView TextView;
+	}
+	
+	final private Activity context;
+	final private MessagePair[] messagePairs;
+	final Timer timer = new Timer();
+	
+	public MessageAnimation(Activity context) {
+		Resources resources = context.getResources();
+
+        MessagePair greetings = new MessagePair();
+        greetings.Message = resources.getString(R.string.greetings);
+        greetings.TextView = (TextView)context.findViewById(R.id.greetings);
+
+        MessagePair message1 = new MessagePair();
+        message1.Message = resources.getString(R.string.message1);
+        message1.TextView = (TextView)context.findViewById(R.id.message1);
+
+        MessagePair message2 = new MessagePair();
+        message2.Message = resources.getString(R.string.message2);
+        message2.TextView = (TextView)context.findViewById(R.id.message2);
+
+        MessagePair message3 = new MessagePair();
+        message3.Message = resources.getString(R.string.message3);
+        message3.TextView = (TextView)context.findViewById(R.id.message3);
+
+        MessagePair signature = new MessagePair();
+        signature.Message = resources.getString(R.string.signature);
+        signature.TextView = (TextView)context.findViewById(R.id.signature);
+
+        this.context = context;
+		this.messagePairs = new MessagePair[] { greetings, message1, message2, message3, signature };
+	}
+	
+	private void clearAllMessage() {
+		for (int i = 0; i < this.messagePairs.length; i++) {
+			messagePairs[i].TextView.setText("");
+		}
+	}
+	
+	public void start() {
+		this.clearAllMessage();
+		
+		final TimerTask task = new TimerTask() {
+			private MessageRunnable currentMessage;
+			private int currentIndex = -1;
+			
+			@Override
+			public void run() {
+				if (this.isRunning()) {
+					context.runOnUiThread(this.currentMessage);
+				} else if (this.hasNext()) {
+					currentMessage = this.createNextRunner();
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
+				}
+				else {
+					timer.cancel();
+				}
+			}
+			
+			private boolean isRunning() {
+				return currentMessage != null && !currentMessage.getFinished();
+			}
+			
+			private boolean hasNext() {
+				if (currentMessage != null && currentMessage.getFinished() ) {
+					if (currentIndex == messagePairs.length - 1) {
+						return false;
+					}
+				}
+				
+				return true;
+			}
+			
+			private MessageRunnable createNextRunner() {
+				currentIndex++;
+				
+				String message = messagePairs[currentIndex].Message;
+				TextView textView = messagePairs[currentIndex].TextView;
+				
+				return new MessageRunnable(message, textView);
+			}
+		};
+		
+		this.timer.schedule(task, 1000, 200);
+	}
+	
+	public void cancel() {
+		this.timer.cancel();
+	}
+	
+	class MessageRunnable implements Runnable {
+		final private char[] messageChars;
+		final private TextView textView;
+		
+		private int displayedLength = 0;
+		
+		public MessageRunnable(String message, TextView textView) {
+			this.messageChars = message.toCharArray();
+			this.textView = textView;
+		}
+		
+		public boolean getFinished() {
+			return this.displayedLength == this.messageChars.length;
+		}
+
+		public void run() {
+			this.displayedLength = this.displayedLength + 1;
+			this.textView.setText(this.messageChars, 0, this.displayedLength);
+		}		
+	}
+}
